ef039aefd6093ec9d5b5e31cc48a7a53952a3307
Paulo Civardi <paulocivardi@gmail.com>
2024-04-10 19:36:36 -0400

    geração de caracteres aleátórios para a formação de senha

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-console.log("teste");
+// Elementos
+const generatePasswordBtn = document.querySelector("#gerar-senha");
+const generatedPasswordElement = document.querySelector("#generator-container");
+
+// Funções
+// Letras, Números e Símbolos
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+    const symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    let password = "";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+// Eventos
+generatePasswordBtn.addEventListener("click", () => {
+    console.log(getLetterLowerCase(), getLetterUperCase(), getNumber(), getSymbol());
+});
